55947d8cb4bc4625b438cf9db79dc5b786a1e21e
H43RO <h2is1234@naver.com>
2022-01-23 23:11:16 +0900

    onSizeChanged() 메소드 정의

--- MiniPaint/app/src/main/java/com/haero_kim/minipaint/MainActivity.kt
+++ MiniPaint/app/src/main/java/com/haero_kim/minipaint/MainActivity.kt
@@ -2,10 +2,19 @@ package com.haero_kim.minipaint
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val myCanvasView = MyCanvasView(this)
+
+        // 그림판을 전체화면으로 사용
+        myCanvasView.systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+        myCanvasView.contentDescription = getString(R.string.canvasContentDescription)
+
+        setContentView(myCanvasView)
+
     }
 }
---
+++ MiniPaint/app/src/main/java/com/haero_kim/minipaint/MyCanvasView.kt
@@ -0,0 +1,33 @@
+package com.haero_kim.minipaint
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.view.View
+import androidx.core.content.res.ResourcesCompat
+
+class MyCanvasView(context: Context) : View(context) {
+
+    private lateinit var extraCanvas: Canvas
+    private lateinit var extraBitmap: Bitmap
+
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+
+    override fun onSizeChanged(width: Int, height: Int, oldWidth: Int, oldHeight: Int) {
+        super.onSizeChanged(width, height, oldWidth, oldHeight)
+
+        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+
+        // width * height 는 결국 디바이스의 화면 사이즈가 될 것
+        extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+
+        extraCanvas = Canvas(extraBitmap)
+        extraCanvas.drawColor(backgroundColor)
+
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(extraBitmap, 0f, 0f, null)
+    }
+}
